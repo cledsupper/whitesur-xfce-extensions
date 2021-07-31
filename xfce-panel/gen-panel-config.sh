ca2d4b8a618fdec93cff000fa8b73bf67908612a
Cledson Ferreira <cledsonitgames@gmail.com>
2021-07-30 21:25:20 -0300

    Script atualizado

--- xfce-panel/gen-panel-config.sh
+++ xfce-panel/gen-panel-config.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 echo "Generating panel configuration..."
+rm *.tar.bz2
 mv config-light.txt config.txt
-tar -cvjSf lesso-whitesur-light.tar.bz2 launcher-12 launcher-2 config.txt
+tar -cvjSf lesso-whitesur-light.tar.bz2 launcher-13 launcher-2 config.txt
 mv config.txt config-light.txt
 mv config-dark.txt config.txt
-tar -cvjSf lesso-whitesur-dark.tar.bz2 launcher-12 launcher-2 config.txt
+tar -cvjSf lesso-whitesur-dark.tar.bz2 launcher-13 launcher-2 config.txt
 mv config.txt config-dark.txt
 
 echo "FINISHED!"
